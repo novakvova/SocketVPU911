cdd305f59d51120b76b22535eae61a76e2c1deaa
novakvova <novakvova@gmail.com>
2021-04-18 11:30:09 +0300

    Simple server and client

---
+++ ConsoleServer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleServer
+{
+    class Program
+    {
+        static int port = 2895; // порт для приема входящих запросов
+        static void Main(string[] args)
+        {
+            // получаем адреса для запуска сокета
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("10.7.150.46"), port);
+
+            // создаем сокет
+            Socket socketA = new Socket(AddressFamily.InterNetwork, 
+                SocketType.Stream, 
+                ProtocolType.Tcp);
+
+            try
+            {
+                // связываем сокет с локальной точкой, по которой будем принимать данные
+                socketA.Bind(ipPoint);
+
+                // начинаем прослушивание
+                socketA.Listen(10);
+
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    //socketSveta
+                    //socketPetro
+                    Socket handler = socketA.Accept();
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байтов
+                    byte[] data = new byte[256]; // буфер для получаемых данных
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+                    // отправляем ответ
+                    string message = "ваше сообщение доставлено";
+                    data = Encoding.Unicode.GetBytes(message);
+                    handler.Send(data);
+                    // закрываем сокет
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        
+        //Console.WriteLine("Hello World!");
+        }
+    }
+}
